7b4936d7fab302de1f74c588cdeef248b185493c
BurakCaglayan <burakkcaglayann@gmail.com>
2021-07-08 00:41:27 +0300

    Add express

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import express from "express";
 
 dotenv.config();
 
-console.log(process.env.PORT);
+const app = express();
+app.get("/api/health-check", (req, res) => {
+  res.json({
+    status: "OK"
+  });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Api running on port ${port}`))
