900556248906ef36f61d497d4c308c4e3556cd41
BurakCaglayan <burakkcaglayann@gmail.com>
2021-07-08 00:45:18 +0300

    Add body-parser

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import dotenv from "dotenv";
 import express from "express";
+import { json } from "body-parser";
 
 dotenv.config();
 
 const app = express();
+
+app.use(json());
+
 app.get("/api/health-check", (req, res) => {
   res.json({
-    status: "OK"
+    status: "OK",
   });
 });
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Api running on port ${port}`))
+app.listen(port, () => console.log(`Api running on port ${port}`));
