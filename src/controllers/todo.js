4c951141a6d8d60174bd9a38812b6320318d68b9
BurakCaglayan <burakkcaglayann@gmail.com>
2021-07-08 01:52:02 +0300

    Implement todo controller methods

--- src/controllers/todo.js
+++ src/controllers/todo.js
@@ -1,18 +1,41 @@
+let todos = [
+  {
+    id: 1,
+    title: "First Todo",
+    description: "First Description",
+    status: "COMPLETED",
+  },
+];
+
 class _TodoController {
   list(req, res) {
-    return res.end("List not implemented yet");
+    return res.json(todos);
   }
+
   get(req, res) {
-    return res.end("Get not implemented yet");
+    return res.json(todos.find((todo) => todo.id == req.params.id));
   }
+
   create(req, res) {
-    return res.end("Create not implemented yet");
+    const id = todos.length > 0 ? todos[todos.length - 1].id + 1 : 1;
+
+    const item = { ...req.body, id };
+    todos.push(item);
+    return res.json(item);
   }
+
   update(req, res) {
-    return res.end("Update not implemented yet");
+    const index = todos.findIndex((todo) => todo.id == req.params.id);
+    if (index === -1) {
+      return res.status("404").end();
+    }
+    todos[index] = { ...todos[index], ...req.body };
+    return res.json(todos[index]);
   }
+
   delete(req, res) {
-    return res.end("Delete not implemented yet");
+    todos = todos.filter((todo) => todo.id != req.params.id);
+    return res.status("204").end();
   }
 }
 
