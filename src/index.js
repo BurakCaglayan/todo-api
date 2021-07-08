a3c5e39bcdb318d5cdfb6fc6144c819afd8baf9e
BurakCaglayan <burakkcaglayann@gmail.com>
2021-07-09 00:07:47 +0300

    Add cors

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import express from "express";
 import { json } from "body-parser";
 import TodoRouter from "./routes/todo";
 import { connectDb } from "./db";
+import cors from 'cors'
 
 dotenv.config();
 
 const app = express();
 
 app.use(json());
+app.use(cors());
 
 app.use("/api/todos", TodoRouter);
 
